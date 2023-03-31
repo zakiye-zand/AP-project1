ef30103edccf3248d243dcd152622fc00bc7e445
Zakiye Zand <zakiyezand@gmail.com>
2023-03-31 13:48:43 +0430

    Base structure

---
+++ code.py
@@ -0,0 +1,86 @@
+def Role():
+     #input the role and pass it to the class
+    pass    
+
+# def Inventory():
+    #needs data base ,to make list of good's code and number and price
+    pass
+def Store(warehouse):
+    #show the list of available ware
+    pass
+# def Update(Inventory):
+    #after each order need to update the list of goods
+    pass
+def Order(Store):
+    # 1. take order name
+    # 2. take order number and property for eachone
+    pass
+def Options():
+    #show  and take options:
+    #           1.store  
+    #           2.cart
+    pass
+def cart():
+    #show orders
+    #calcute the price
+    #make sure with showing option:
+    #1. complete the purchase
+    #2. back to store
+    #3.delete good(unneeded)
+    pass
+def Delivery_time():
+    # needs data base to calcute what time to show
+    # show the time and take one    
+    pass
+
+def Specification(Logistic):
+    #use get address of logistic
+    # fuul name , number , addres , delivery time
+    # go to portal
+    pass
+def Portal():
+    # this is a class:...
+    # make  random purchase number 11digit
+    # get card number 16 digit and check
+    # print confirmation by the way as .txt
+    # print Invoice if was ok
+    pass
+
+class Warehouse():
+    #make a data frame |Wh.number|W.code|size|number
+    def update():
+        #update inventory after successfull purchase automaticly
+        pass
+    def manualy():
+        # options: 1.add 2.update
+        # 1.csv file  2.manualy
+        pass
+    def Output():
+        #with csv    
+        pass     
+
+class Accounting():
+    def update():
+        #update the data frame
+       pass 
+    def Output():
+        pass
+
+class Logistic:
+    def __init__(self) -> None:
+        #need a data frame of number of orders by the time and method
+        pass
+    def get_address():
+        #input the address and check them
+        pass
+    def Delivery_method():
+        #alloacate a method by city
+        pass
+    def Delivery_time():
+        #show available time by data frame of that
+        # take a time from user
+        pass
+    def Update():
+        #uppdate the data frame by method and chosen time
+        pass    
+
