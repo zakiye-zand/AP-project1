6189601df0e71f884a9131c507c4798189941301
Zakiye Zand <zakiyezand@gmail.com>
2023-05-07 00:00:35 +0430

    edit.4

--- code.py
+++ code.py
@@ -157,17 +157,18 @@ class Logistic:
     def Delivery_time(self):    # shows available times and takes one #
 
         print("\nChoose the suitable post delivery time \n\n1.morning ")
-        if self.ds.iloc[0, 1] > 0:
+        if self.ds.iloc[1, 1] < 3:
             print("2.noon")
-        if self.ds.iloc[1, 1] > 0:
+        if self.ds.iloc[2, 1] < 3:
             print("3.evening\n")
         self.o = eval(input("option : "))
-        return [self.ds.iloc[self.o-2, 0], self.o-2]
+
+        return [self.ds.iloc[self.o-1, 0], self.o-1]
 
     def Update(time_index: int):
         ds = pd.read_csv(
             "C:/Users/LENOVO/Desktop/Ap/project 1/Delivery schedual.csv")      # uppdate the schedual chosen time #
-        ds.iloc[time_index, 1] -= 1
+        ds.iloc[time_index, 1] += 1
         ds.to_csv(
             "C:/Users/LENOVO/Desktop/Ap/project 1/Delivery schedual.csv", index=False)
         pass
