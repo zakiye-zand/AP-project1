2b0c2f6cd628a0b34b97b7bca669e29d6292992a
Zakiye Zand <zakiyezand@gmail.com>
2023-05-06 12:20:05 +0430

    edit.2

--- code.py
+++ code.py
@@ -2,7 +2,7 @@ import pandas as pd
 from random import randint
 
 
-class Role():
+class Role():   #choosing the role and managment section
     def role():
         o = input("Choose your Role\n1.customer\n2.manager\n")
         return o
@@ -18,31 +18,34 @@ class Warehouse:
         self.d = pd.read_csv(
             'C:/Users/LENOVO/Desktop/Ap/project 1/warehouse.csv')
 
-    def Option(self):
+    def Option(self): # gets what user wants to do
         print("***Warehouse managment***\n")
         o = input("1.update list of goods\n2.print list of goods\n")
-        # if o == '1':
-        #     self.Manualy_update()
-        # elif o == '2':
-        #     self.Output()
         return o
 
-    def Update(name, number: int):
-        d = pd.read_csv(
-            'C:/Users/LENOVO/Desktop/Ap/project 1/warehouse.csv')
-        d.loc[(d['name'] == name), 'number'] = number
-        d.to_csv(
+    def auto_update(self , name , number):      # changes the warehouse file by name and *lost number of wares
+        self.d.loc[(self.d['name'] == name), 'number'] -= number
+        self.d.to_csv(
             "C:/Users/LENOVO/Desktop/Ap/project 1/warehouse.csv", index=False)
         pass
 
-    def Manualy_update(self):
+    def Update( self , name , number: int): #changes the warehouse file by name and  *new number of wares
+        # d = pd.read_csv(
+            # 'C:/Users/LENOVO/Desktop/Ap/project 1/warehouse.csv')
+        self.d.loc[(self.d['name'] == name), 'number'] = number
+        self.d.to_csv(
+            "C:/Users/LENOVO/Desktop/Ap/project 1/warehouse.csv", index=False)
+        pass
+
+    def Manualy_update(self): #Not automatic , this is how manager access for update
 
         o = input(
-            "Choose update method\n1.csv file address \n2.enter the code and number of goods\n")
+            "Choose update method\n1.csv file address \n2.enter the name and number of goods\n")
 
         if o == '2':
-            self.Update(eval(input("enter the code\n")),
-                        eval(input("enter the new number\n")))
+            name = input("enter the name\n")
+            number = eval(input("enter the new number\n"))
+            warehouse.Update(name ,number)
         elif o == '1':
             path = input("enter the path of csv file \n")
             d = pd.read_csv(path)
@@ -53,7 +56,7 @@ class Warehouse:
 
         pass
 
-    def Output(self):
+    def Output(self): # saves an output of wares
         d = self.d[['wh.code', 'code', 'number']]
         d.to_csv(
             "c:/Users/LENOVO/Desktop/Ap/project 1/warehouse output.csv", index=False)
@@ -61,35 +64,31 @@ class Warehouse:
 
 class Store():
 
-    def Order(wares: list):  # add the orders to the list
+    def Order(wares: list):     # shows goods and takes order and add the order to ware list #
         d = pd.read_csv(
             'C:/Users/LENOVO/Desktop/Ap/project 1/warehouse.csv')
         for i in d.index:
-            if d.loc[i, 'number'] > 0:
+            if d.loc[i, 'number'] > 0:      # shows available goods #
                 print(
                     f"{i+1}.({d.loc[i , 'number']}){d.loc[i , 'name']} {d.loc[i , 'price']}")
             else:
-                print(f"{i+1}.{d.loc[i , 'name']}\t unavailable")
+                print(f"{i+1}.{d.loc[i , 'name']}  unavailable")
 
         o = eval(input("please choose the good you want: "))
         n = eval(input("please choose the number you want: "))
-        if d.loc[o-1, 'number'] >= n:
+
+        if d.loc[o-1, 'number'] >= n:       # checks if the chosen number of a good be in warehouse #
             s = input("Wich size do you want?(M,L,XL) ")
-            print("\nthe goods added to your cart successfuly\n")
+            print("\nthe goods added to your cart successfuly:)")
             l = [d.loc[o-1, 'name'], d.loc[o-1, 'price'], n]
             wares.append(l)
-
         else:
             print("Oops! the number you want is not available")
-
-        print("Choose an option")
-        o = input("1.Back to store\n2.cart\n")
-        if o == '1':
-            Store.Order()
         return wares
+    
 
-    def cart(wares):
-        print(wares)
+    def cart(wares):    #shows all chosen goods and let to add more 
+        print(f"\n{wares}\n")
         print("Choose an option to continue!")
         o = input("1.Back to store\n2.Payment\n")
         return o
@@ -101,7 +100,7 @@ class Logistic:
             "C:/Users/LENOVO/Desktop/Ap/project 1/Delivery schedual.csv")
         pass
 
-    def get_address(self):      # gets and check the address #
+    def get_address(self):      # gets and checks and returns the address #
         buyer_address = []
 
         while (True):
@@ -140,7 +139,7 @@ class Logistic:
             method = 'Post'
         return method
 
-    def Delivery_time(self):    # shows available times and take one #
+    def Delivery_time(self):    # shows available times and takes one #
 
         print("\nChoose the suitable post delivery time \n\n1.morning ")
         if self.ds.iloc[0, 1] > 0:
@@ -148,12 +147,12 @@ class Logistic:
         if self.ds.iloc[1, 1] > 0:
             print("3.evening\n")
         self.o = eval(input("option : "))
-        return [self.ds.loc[self.o-1, 'time'], o-1]
+        return [self.ds.iloc[self.o-2, 0], self.o-2]
 
-    def Update(time):
+    def Update(time_index:int):
         ds = pd.read_csv(
             "C:/Users/LENOVO/Desktop/Ap/project 1/Delivery schedual.csv")      # uppdate the schedual chosen time #
-        ds.loc[time, 1] -= 1
+        ds.iloc[time_index, 1] -= 1
         ds.to_csv(
             "C:/Users/LENOVO/Desktop/Ap/project 1/Delivery schedual.csv", index=False)
         pass
@@ -164,7 +163,7 @@ class Payment():
         self.wares = wares
         self.buyer = buyer
 
-    def get_info(self):
+    def get_info(self):         # gets all needed information with calling logistick and returns that as buyer information #
 
         first_name = input("enter your first name : ")
         last_name = input("enter your last name : ")
@@ -181,7 +180,7 @@ class Payment():
                       address, delivery_method, delivery_time, purchaseNo]
         return self.buyer
 
-    def confirmation(self):
+    def confirmation(self):         # saves a txt file for confirmation #
 
         f = open(
             f"C:/Users/LENOVO/Desktop/Ap/project 1/configration_{self.buyer[6]}.txt", mode="w")
@@ -191,7 +190,7 @@ class Payment():
             f.write(
                 f"Sorry! Your purchase No.{self.buyer[6]} wasn't successfull!")
         f.write(
-            f"Name :{self.buyer[0]} {self.buyer[1]} \n Card number :{self.card}")
+            f"\nName :{self.buyer[0]} {self.buyer[1]} \nCard number :{self.card}")
         f.close()
         print(f"Your purchase No.{self.buyer[6]} conformation file  saved.")
         if len(self.card) != 16:
@@ -200,7 +199,7 @@ class Payment():
         else:
             return 1
 
-    def Invoice(self):
+    def Invoice(self):      # prints invoice as txt if purchase be successfull
 
         f = open(
             f"C:/Users/LENOVO/Desktop/Ap/project 1/Invoice_{self.buyer[6]}.txt", mode="w")
@@ -215,7 +214,7 @@ class Payment():
         f.close()
 
 
-class Accounting():
+class Accounting():         # updates accounting file after each successful purchase and saves it #
 
     def Update(wares: list, buyer: list):
         d = pd.read_csv(
@@ -224,7 +223,7 @@ class Accounting():
         price = 0
         for i in wares:
             number += i[2]
-            price += eval(i[1])
+            price += eval(i[1][:-1])
         d.loc[len(d.index)] = [buyer[6], number, price, 30000, price*9.0/100]
         d.to_csv(
             "c:/Users/LENOVO/Desktop/Ap/project 1/accounting.csv", index=False)
@@ -235,25 +234,25 @@ class Accounting():
         pass
 
 
-print("***Welcome***")
+print("***Welcome to Felona store***")
 
 o = Role.role()
 if o == '1':
     wares = []  # [[name , price , number]]
-    wares = Store.Order(wares)
-    o = Store.cart(wares)
-    if o == '1':
+    while o != '2':
         wares = Store.Order(wares)
-    elif o == '2':
-        buyer = []  # [first name, last name, phone number, [State, City, Details, PostCode], delivery method, [delivery time, index], purchase number]
-        payment = Payment(wares, buyer)
-        buyer = Payment.get_info(payment)
-        if Payment.confirmation(payment) == 1:
-            Payment.Invoice(payment)
-            print("Purchase completed successfully.\n your invoice file saved")
-            Logistic.Update(buyer[5][1])
-            Warehouse.Update(wares[0], wares[2])
-            Accounting.Update(wares,buyer)
+        o = Store.cart(wares)
+    buyer = []  # [first name, last name, phone number, [State, City, Details, PostCode], delivery method, [delivery time, index], purchase number]
+    payment = Payment(wares, buyer)
+    buyer = Payment.get_info(payment)
+    if Payment.confirmation(payment) == 1:
+        Payment.Invoice(payment)
+        print("Purchase completed successfully.\nYour invoice file saved\nThanks for trusting us:)")
+        Logistic.Update(buyer[5][1])
+        warehouse = Warehouse()
+        for i in wares:
+            Warehouse.auto_update(warehouse , i[0], i[2])
+        Accounting.Update(wares,buyer)
 
 
 elif o == '2':
